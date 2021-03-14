684ed5c2dbb70818300276eaced48898f5ace228
Girma Ebssa <girma.ebssa@gmail.com>
2021-03-14 10:38:50 -0700

    adds experience component with its styling

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -28,7 +28,7 @@ const Experience = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     </div>
                 </div>
-                <div className="timeline-block timeline-block-left">
+                <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
                         <h3>2021-present</h3>
